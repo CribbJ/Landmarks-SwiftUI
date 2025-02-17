29ec215d501a31be59f9f048cc855998bde7d505
CribbJ <cribbj32@gmail.com>
2025-02-17 23:45:33 +0000

    First commit through x-code. Following SwiftUI from location https://developer.apple.com/tutorials/swiftui

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by James Cribb on 17/02/2025.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
